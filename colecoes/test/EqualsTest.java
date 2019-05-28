577f9412955f12ec7622a4c6f95c0f9d722720e0
Matheuskbrasil <matheus.070202@gmail.com>
2019-05-28 17:22:22 -0300

    10%

---
+++ colecoes/test/EqualsTest.java
@@ -0,0 +1,19 @@
+package colecoes.test;
+
+import colecoes.classes.Celular;
+
+ 
+public class EqualsTest {
+    public static void main(String[] args) {
+        String nome1 = "Brbrbr Matheus";
+        String nome2 = new String("Matheus Brbrbr");
+        Integer int1 = 5;
+        Integer int2 = new Integer(5);
+        System.out.println(nome1.equals(nome2));
+        System.out.println(int1.equals(int2));
+
+        Celular c1 = new Celular("iPhone", "1234");
+        Celular c2 = new Celular("iPhone", "1234");
+        System.out.println(c2.equals(c1));
+    }
+}
