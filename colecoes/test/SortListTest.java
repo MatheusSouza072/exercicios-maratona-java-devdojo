1140b7e6704d8db297fb26e8ac49dca95768dba0
Matheuskbrasil <matheus.070202@gmail.com>
2019-05-29 16:24:38 -0300

    90

---
+++ colecoes/test/SortListTest.java
@@ -0,0 +1,32 @@
+package colecoes.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortListTest {
+	public static void main(String[] args) {
+		List<String> nomes = new ArrayList<>();
+		nomes.add("Matheus");
+		nomes.add("Souza");
+		nomes.add("DevDojo");
+		nomes.add("Brenon");
+		nomes.add("Bruno");
+		nomes.add(0, "Ana");
+		Collections.sort(nomes);
+		List<Double> numeros = new ArrayList<>();
+		numeros.add(1.5);
+		numeros.add(1.3);
+		numeros.add(1.9);
+		numeros.add(2d);
+		Collections.sort(numeros);
+		for (String nome : nomes) {
+			System.out.println(nome);
+		}
+
+		for (Double numero : numeros) {
+			System.out.println(numero);
+		}
+
+	}
+}
